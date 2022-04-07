08ec18a2805731ee48f499cc45c8326c6d342520
C0NN3CT10N-L0ST <alexandreromba@hotmail.com>
2022-04-07 15:52:25 +0100

    Updates 'Pessoa' class

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -2,4 +2,12 @@ public class Pessoa {
     int id;
     String nome;
     Character genero;
+
+    Pessoa() {}
+
+    Pessoa(int id, String nome, Character genero) {
+        this.id = id;
+        this.nome = nome;
+        this.genero = genero;
+    }
 }

112c2f1c1324ac21ca59468837363d3a902542ed
C0NN3CT10N-L0ST <alexandreromba@hotmail.com>
2022-04-14 05:36:52 +0100

    Adds mandatory functions' signature and implements 'getIgnoredLines' function

--- src/pt/ulusofona/deisi/aed/deisiflix/Main.java
+++ src/pt/ulusofona/deisi/aed/deisiflix/Main.java
@@ -1,7 +1,30 @@
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
+    static void lerFicheiros() throws IOException {
+        // TODO
+    }
+
+    static ArrayList<Filme> getFilmes() {
+        // TODO
+    }
+
+    static ArrayList<String> getLinhasIgnoradas(String fileName) throws IOException {
+        switch (fileName) {
+            case "deisi_movies.txt":
+            case "deisi_people.txt":
+                return Reader.getIgnoredLines(fileName, 5);
+            case "deisi_movie_votes.txt":
+                return Reader.getIgnoredLines(fileName, 3);
+            case "deisi_genres.txt":
+                return Reader.getIgnoredLines(fileName, 2);
+            default:
+                return null;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        Reader.genresReader();
+        // Reader.genresReader();
     }
 }
--- src/pt/ulusofona/deisi/aed/deisiflix/Reader.java
+++ src/pt/ulusofona/deisi/aed/deisiflix/Reader.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Reader {
     static boolean DEBUG = true;
@@ -11,28 +12,28 @@ public class Reader {
         FileReader fr = new FileReader(testDBPath + "deisi_movies.txt");
         BufferedReader reader = new BufferedReader(fr);
 
-        String linha = null;
+        String line = null;
 
-        while ((linha = reader.readLine()) != null) {
+        while ((line = reader.readLine()) != null) {
             if (DEBUG) {
-                System.out.println("Line -> " + linha);
+                System.out.println("Line -> " + line);
             }
 
-            String dados[] = linha.split(",");
+            String components[] = line.split(",");
 
-            if (dados.length == 5) {
-                int id = Integer.parseInt(dados[0].strip());
-                String titulo = dados[1].strip();
-                int duracao = Integer.parseInt(dados[2].strip());
-                int orcamento = Integer.parseInt(dados[3].strip());
-                String data = dados[4].strip();
+            if (components.length == 5) {
+                int id = Integer.parseInt(components[0].strip());
+                String title = components[1].strip();
+                int duration = Integer.parseInt(components[2].strip());
+                int budget = Integer.parseInt(components[3].strip());
+                String date = components[4].strip();
 
                 if (DEBUG) {
                     System.out.println("ID: " + id);
-                    System.out.println("Title: "+ titulo);
-                    System.out.println("Duration: " + duracao);
-                    System.out.println("Budget " + orcamento);
-                    System.out.println("Date: " + data);
+                    System.out.println("Title: "+ title);
+                    System.out.println("Duration: " + duration);
+                    System.out.println("Budget " + budget);
+                    System.out.println("Date: " + date);
                     System.out.println("-----------".repeat(6));
                 }
             }
@@ -47,24 +48,24 @@ public class Reader {
         FileReader fr = new FileReader(testDBPath + "deisi_movie_votes.txt");
         BufferedReader reader = new BufferedReader(fr);
 
-        String linha = null;
+        String line = null;
 
-        while ((linha = reader.readLine()) != null) {
+        while ((line = reader.readLine()) != null) {
             if (DEBUG) {
-                System.out.println("Line -> " + linha);
+                System.out.println("Line -> " + line);
             }
 
-            String dados[] = linha.split(",");
+            String components[] = line.split(",");
 
-            if (dados.length == 3) {
-                int id = Integer.parseInt(dados[0].strip());
-                float mediaVotos = Float.parseFloat(dados[1].strip());
-                int totalVotos = Integer.parseInt(dados[2].strip());
+            if (components.length == 3) {
+                int id = Integer.parseInt(components[0].strip());
+                float votesAverage = Float.parseFloat(components[1].strip());
+                int votesTotal = Integer.parseInt(components[2].strip());
 
                 if (DEBUG) {
                     System.out.println("ID: " + id);
-                    System.out.println("Vote Average: "+ mediaVotos);
-                    System.out.println("Nr. Votes: " + totalVotos);
+                    System.out.println("Vote Average: "+ votesAverage);
+                    System.out.println("Nr. Votes: " + votesTotal);
                     System.out.println("-----------".repeat(6));
                 }
             }
@@ -79,28 +80,28 @@ public class Reader {
         FileReader fr = new FileReader(testDBPath + "deisi_actors.txt");
         BufferedReader reader = new BufferedReader(fr);
 
-        String linha = null;
+        String line = null;
 
-        while ((linha = reader.readLine()) != null) {
+        while ((line = reader.readLine()) != null) {
             if (DEBUG) {
-                System.out.println("Line -> " + linha);
+                System.out.println("Line -> " + line);
             }
 
-            String dados[] = linha.split(",");
+            String components[] = line.split(",");
 
-            if (dados.length == 5) {
-                String tipo = dados[0].strip();
-                int idPessoa = Integer.parseInt(dados[1].strip());
-                String nome = dados[2].strip();
-                String genero = dados[3].strip();
-                int idFilme = Integer.parseInt(dados[4].strip());
+            if (components.length == 5) {
+                String tipo = components[0].strip();
+                int idPerson = Integer.parseInt(components[1].strip());
+                String name = components[2].strip();
+                String genre = components[3].strip();
+                int idMovie = Integer.parseInt(components[4].strip());
 
                 if (DEBUG) {
                     System.out.println("Tipe Person: " + tipo);
-                    System.out.println("ID: " + idPessoa);
-                    System.out.println("Name: "+ nome);
-                    System.out.println("Genre: " + genero);
-                    System.out.println("ID Movie: " + idFilme);
+                    System.out.println("ID: " + idPerson);
+                    System.out.println("Name: "+ name);
+                    System.out.println("Genre: " + genre);
+                    System.out.println("ID Movie: " + idMovie);
                     System.out.println("-----------".repeat(6));
                 }
             }
@@ -115,21 +116,21 @@ public class Reader {
         FileReader fr = new FileReader(testDBPath + "deisi_genres.txt");
         BufferedReader reader = new BufferedReader(fr);
 
-        String linha = null;
+        String line = null;
 
-        while ((linha = reader.readLine()) != null) {
+        while ((line = reader.readLine()) != null) {
             if (DEBUG) {
-                System.out.println("Line -> " + linha);
+                System.out.println("Line -> " + line);
             }
 
-            String dados[] = linha.split(",");
+            String components[] = line.split(",");
 
-            if (dados.length == 2) {
-                String genero = dados[0].strip();
-                int id = Integer.parseInt(dados[1].strip());
+            if (components.length == 2) {
+                String genre = components[0].strip();
+                int id = Integer.parseInt(components[1].strip());
 
                 if (DEBUG) {
-                    System.out.println("Genre Name: " + genero);
+                    System.out.println("Genre Name: " + genre);
                     System.out.println("ID Movie: " + id);
                     System.out.println("-----------".repeat(6));
                 }
@@ -139,5 +140,31 @@ public class Reader {
         }
 
         reader.close();
+    }
+
+    public static ArrayList<String> getIgnoredLines(String fileName, int numberOfComponents) throws IOException {
+        FileReader fr = new FileReader(testDBPath + fileName);
+        BufferedReader reader = new BufferedReader(fr);
+
+        ArrayList<String> ignoredLines = new ArrayList<>();
+
+        String line = null;
+
+        while ((line = reader.readLine()) != null) {
+            if (DEBUG) {
+                System.out.println("Line -> " + line);
+            }
+
+            String components[] = line.split(",");
+
+            if (components.length != numberOfComponents) {
+                ignoredLines.add(line);
+            }
+
+        }
+
+        reader.close();
+
+        return ignoredLines;
     }
 }

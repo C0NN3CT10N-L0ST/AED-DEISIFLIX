3d230839df23edc7e003fe7d758af85e246cb03a
C0NN3CT10N-L0ST <alexandreromba@hotmail.com>
2022-05-25 17:53:19 +0100

    Adds 'QueryResult' class

---
+++ src/pt/ulusofona/deisi/aed/deisiflix/QueryResult.java
@@ -0,0 +1,6 @@
+package pt.ulusofona.deisi.aed.deisiflix;
+
+public class QueryResult {
+    String valor;   // Valor calculado pela query
+    long tempo;     // Tempo que a query demorou a executar (ms)
+}

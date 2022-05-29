132f679af8815c6b9d638cb873e51af41da37704
C0NN3CT10N-L0ST <alexandreromba@hotmail.com>
2022-05-29 06:35:43 +0100

    Updates 'MoviesData' class to support 'sortedMovies' array

--- src/pt/ulusofona/deisi/aed/deisiflix/MoviesData.java
+++ src/pt/ulusofona/deisi/aed/deisiflix/MoviesData.java
@@ -3,13 +3,15 @@ package pt.ulusofona.deisi.aed.deisiflix;
 import java.util.ArrayList;
 
 public class MoviesData {
-    ArrayList<Filme> movies;
+    ArrayList<Filme> moviesFileOrder;
+    Filme[] sortedMovies;
     ArrayList<String> ignoredLines;
 
     MoviesData() {}
 
-    MoviesData(ArrayList<Filme> movies, ArrayList<String> ignoredLines) {
-        this.movies = movies;
+    MoviesData(ArrayList<Filme> moviesFileOrder, Filme[] sortedMovies, ArrayList<String> ignoredLines) {
+        this.moviesFileOrder = moviesFileOrder;
+        this.sortedMovies = sortedMovies;
         this.ignoredLines = ignoredLines;
     }
 }

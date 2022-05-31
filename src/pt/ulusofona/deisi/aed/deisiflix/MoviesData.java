79b03e784414e860ad64faac2cc7eceba3906326
C0NN3CT10N-L0ST <alexandreromba@hotmail.com>
2022-05-31 23:06:17 +0100

    Adds 'HashMap' to store movies by year to 'MoviesData'

--- src/pt/ulusofona/deisi/aed/deisiflix/MoviesData.java
+++ src/pt/ulusofona/deisi/aed/deisiflix/MoviesData.java
@@ -1,17 +1,23 @@
 package pt.ulusofona.deisi.aed.deisiflix;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MoviesData {
     ArrayList<Filme> moviesFileOrder;
     Filme[] sortedMovies;
     ArrayList<String> ignoredLines;
+    HashMap<Integer, ArrayList<Integer>> movieIDsByYear;
 
     MoviesData() {}
 
-    MoviesData(ArrayList<Filme> moviesFileOrder, Filme[] sortedMovies, ArrayList<String> ignoredLines) {
+    MoviesData(ArrayList<Filme> moviesFileOrder,
+               Filme[] sortedMovies,
+               HashMap<Integer, ArrayList<Integer>> movieIDsByYear,
+               ArrayList<String> ignoredLines) {
         this.moviesFileOrder = moviesFileOrder;
         this.sortedMovies = sortedMovies;
+        this.movieIDsByYear = movieIDsByYear;
         this.ignoredLines = ignoredLines;
     }
 }

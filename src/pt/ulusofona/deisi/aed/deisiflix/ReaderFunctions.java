0d83d14f9e7239f8307290ee53722aa308327c94
C0NN3CT10N-L0ST <alexandreromba@hotmail.com>
2022-06-07 13:49:10 +0100

    Adds 'addMovieAssociateToPeople' function to be used in people reader

--- src/pt/ulusofona/deisi/aed/deisiflix/ReaderFunctions.java
+++ src/pt/ulusofona/deisi/aed/deisiflix/ReaderFunctions.java
@@ -11,7 +11,7 @@ public class ReaderFunctions {
      * @param person Pessoa object to be added
      * @param personType Person type
      * @param movieID ID of the movie that were the object will be added
-     * @param movies Array with all movies (sorted by ID)
+     * @param movies HashMap (KEY: movie ID, VALUE: 'Filme' object) with all movies
      */
     public static void addPersonToMovies(
             Pessoa person, String personType, int movieID, HashMap<Integer, Filme> movies
@@ -39,6 +39,61 @@ public class ReaderFunctions {
                 // Adds person to 'realizadores'
                 movie.realizadores.add(person);
             }
+        }
+    }
+
+    /**
+     * Adds 'MovieAssociate' object to an HashMap
+     * (KEY: MovieAssociate Name,VALUE: ArrayList of MovieAssociate's
+     * in which the key corresponds to the name property of the 'MovieAssociate' object)
+     * @param person The given 'MovieAssociate' object
+     * @param people HashMap (KEY: Person Name, VALUE: ArrayList with all the people with the same name as the key)
+     * @param lineNum The line number in which the given person entry is in the original file
+     * @param duplicateLinesByYear HashMap (KEY: Year, VALUE: ArrayList with all the duplicate lines
+     *                             in the following format '<lineNum>:<personID>:<movieID>')
+     * @param moviesDict HashMap (KEY: movie ID, VALUE: 'Filme' object) with all movies
+     */
+    public static void addMovieAssociateToPeople(
+            MovieAssociate person,
+            HashMap<String, ArrayList<MovieAssociate>> people,
+            int lineNum,
+            HashMap<Integer, ArrayList<String>> duplicateLinesByYear,
+            HashMap<Integer, Filme> moviesDict
+    ) {
+        // Checks if an entry for the person name already exists
+        if (people.containsKey(person.name)) {
+            // Checks if 'person' is already present in the 'ArrayList'
+            // (returns -1 if it isn't present, the index in the ArrayList if it is)
+            int personExists = -1;
+            for (int i = 0; i < people.get(person.name).size(); i++) {
+                if (people.get(person.name).get(i).id == person.id) {
+                    personExists = i;
+                }
+            }
+
+            // If person does not exists, adds it to the 'ArrayList'
+            if (personExists == -1) {
+                people.get(person.name).add(person);
+            } else {
+                // If the person does exist, just adds the current movie to it in case it is not there yet
+
+                // Gets movieID to be added to the current 'person' 'associatedMovieID'
+                int movieID = person.associatedMoviesID.get(0);
+
+                // Adds the movie to movieIDs list in case it does not yet exist
+                if (people.get(person.name).get(personExists).associatedMoviesID.contains(movieID)) {
+                    addLineToDuplicateLinesByYear(lineNum, movieID, person.id, duplicateLinesByYear, moviesDict);
+                } else {
+                    people.get(person.name).get(personExists).associatedMoviesID.add(movieID);
+                }
+            }
+        } else {
+            // If the 'HashMap' does not yet contain the key, create a new one and add the current 'person' object
+            ArrayList<MovieAssociate> peopleWithSameName = new ArrayList<>();
+            peopleWithSameName.add(person);
+
+            // Creates new entry with 'person' name
+            people.put(person.name, peopleWithSameName);
         }
     }
 
